a201c45c04f9d91d8ac80c0424546385c21e2899
Sorawich888 <80507673+Sorawich888@users.noreply.github.com>
2024-03-10 23:15:28 +0700

    update

---
+++ src/SecondSemester.tsx
@@ -0,0 +1,13 @@
+// SecondSemester.tsx
+import React from 'react';
+
+const SecondSemester: React.FC = () => {
+  return (
+    <div>
+      <h1>Second Semester Page</h1>
+      <p>This is the page for the second semester.</p>
+    </div>
+  );
+};
+
+export default SecondSemester;
---
+++ src/SummerSemester.tsx
@@ -0,0 +1,13 @@
+// ThirdSemester.tsx
+import React from 'react';
+
+const ThirdSemester: React.FC = () => {
+  return (
+    <div>
+      <h1>Third Semester Page</h1>
+      <p>This is the page for the third semester.</p>
+    </div>
+  );
+};
+
+export default ThirdSemester;
